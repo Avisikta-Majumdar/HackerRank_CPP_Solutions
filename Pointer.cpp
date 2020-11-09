ae9bc57e5641b445cf241ac965fc31f2c492acd9
avisikta19 <47344480+avisikta19@users.noreply.github.com>
2020-11-10 00:17:31 +0530

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+void update(int *a,int *b) {
+    int temp = *a+*b;
+    *b = abs(*a - *b);
+    *a = temp;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+    return 0;
+}
